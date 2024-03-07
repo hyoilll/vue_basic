50d74121a72cfd1545c30077176dc8dbd9155b22
hyoil Lee <dlgydlf123@gmail.com>
2024-03-07 23:53:00 +0900

    入門：クイックスタート

--- src/views/PracticeView.vue
+++ src/views/PracticeView.vue
@@ -1,6 +1,17 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const count = ref(0)
+
+const increment = () => (count.value += 1)
+</script>
+
 <template>
   <div class="practice">
     <h1>This is an practice page</h1>
+    <div id="cnt">
+      <button @click="increment">Count is: {{ count }}</button>
+    </div>
   </div>
 </template>
 
