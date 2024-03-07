53d1522389e5c78a62c00456bd71058312944e9f
hyoil Lee <dlgydlf123@gmail.com>
2024-03-07 23:18:29 +0900

    create: PracticeView

---
+++ src/views/PracticeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="practice">
+    <h1>This is an practice page</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .practice {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
