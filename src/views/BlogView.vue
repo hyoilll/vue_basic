f0cfc26fbf37f097b26ebfbf1a9cf51b66b2a1cf
hyoil Lee <dlgydlf123@gmail.com>
2024-03-22 12:05:26 +0900

    route objからurl情報を取得する

--- src/views/BlogView.vue
+++ src/views/BlogView.vue
@@ -1,3 +1,28 @@
+<script setup>
+import { onMounted, onUnmounted, watch } from 'vue'
+import { useRoute } from 'vue-router'
+const route = useRoute()
+console.log(route?.params?.id)
+
+// このコンポーネントはunMountされないため、watchを使って新しいページに移動したことを検知する。
+watch(route, () => {
+  console.log('watch: ', route?.params?.id)
+})
+
+onMounted(() => {
+  console.log('onMounted')
+})
+onUnmounted(() => {
+  console.log('onUnmounted')
+})
+</script>
+
 <template>
-  <div>Blog</div>
+  <!-- グローバル変数の$routeを使ってURLのparams情報が取得できる。 -->
+  <div>Blog (id:{{ $route?.params?.id }})</div>
+  <!-- 以下のボタンを押下して次のページに移動したとしても、このコンポーネントがunMountされて再度mountされることはない。 -->
+  <!-- onMounted()とonUnmounted()で確認する -->
+  <RouterLink :to="{ name: 'blog', params: { id: Number($route?.params?.id) + 1 } }"
+    >Next</RouterLink
+  >
 </template>
