8d54c6123904799e10528ba0073097d9c463dd22
hyoil Lee <dlgydlf123@gmail.com>
2024-03-24 18:29:10 +0900

    RouterViewをTransitionさせるやり方

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,15 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+/**
+ * <RouterView />はシンプルな使い方で、URLに基づいて自動的にコンポーネントを表示します。
+ * カスタマイズはできませんが、多くの場合にはこれで十分です。
+ *
+ * <RouterView v-slot="{ Component }">は、ルートにマッチしたコンポーネントに対して追加のカスタマイズを行いたい場合に使用します。
+ * これにより、より複雑なレイアウトやアニメーション、条件付きのロジックを適用できますが、設定が少し複雑になります。
+ *
+ * Next Blogに移動した時には同じBlogViewコンポーネントを使い回しているため、Transitionが適用されないように見える。(mountされ、unMountされない）
+ * 同じコンポーネントを使い回さ煮ようにするために、key属性を加える。
+ */
+</script>
 
 <template>
   <header>Vue Router</header>
@@ -8,6 +19,25 @@
       |
       <RouterLink :to="{ name: 'blog', params: { id: 3 } }">Blog</RouterLink>
     </nav>
-    <RouterView />
+    <RouterView v-slot="{ Component, route }">
+      <Transition mode="out-in">
+        <component :is="Component" :key="route.path" />
+      </Transition>
+    </RouterView>
+    <!-- <Transition mode="out-in">
+      <RouterView />
+    </Transition> -->
   </main>
 </template>
+
+<style>
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+</style>
--- src/views/BlogView.vue
+++ src/views/BlogView.vue
@@ -1,13 +1,24 @@
 <script setup lang="ts">
+import { onMounted, onUnmounted } from 'vue'
+
 const props = defineProps({
   id: {
     type: String,
     required: true
   }
 })
+
+onMounted(() => {
+  console.log('onMounted')
+})
+onUnmounted(() => {
+  console.log('onUnmounted')
+})
 </script>
 
 <template>
-  <h2>Blog(id: {{ props.id }})</h2>
-  <RouterLink :to="{ name: 'blog', params: { id: Number(props.id) + 1 } }">Next Blog</RouterLink>
+  <div>
+    <h2>Blog(id: {{ props.id }})</h2>
+    <RouterLink :to="{ name: 'blog', params: { id: Number(props.id) + 1 } }">Next Blog</RouterLink>
+  </div>
 </template>
